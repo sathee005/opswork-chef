473e3329546309b9188488eb34c55abec7dbf199
root <root@ip-172-31-8-161.us-west-1.compute.internal>
2015-03-12 10:26:32 +0000

    just playing around

---
+++ docker-customise/recipes/default.rb
@@ -0,0 +1,18 @@
+include_recipe "apt"
+
+package 'nginx' do
+  action :install
+end
+
+service 'nginx' do
+  action [ :enable, :start ]
+end
+
+cookbook_file "/usr/share/nginx/html/index.html" do
+  source "index.html"
+  mode "0644"
+end
+
+include_recipe 'docker'
+
+docker_image 'samalba/docker-registry'
