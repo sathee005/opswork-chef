06808eb5430c71a8a1af3c3416164259e16aabc6
root <root@ip-172-31-25-141.us-west-1.compute.internal>
2015-03-17 10:03:18 +0000

    fixed attribute error

---
+++ passenger/attributes/default.rb
@@ -0,0 +1,3 @@
+default["passenger"]["clonelocation"] = "/home/ubuntu/test"
+default["passenger"]["repourl"] = "https://github.com/RailsApps/rails-signup-download.git"
+default["passenger"]["rubyversion"]= "ruby1.9.3"
